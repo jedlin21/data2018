17a7a3365187edc17b8d835c4fb82289b59aa819
I <jedlikowski.pat@gmail.com>
2018-02-07 15:25:58 +0100

    Completion / Unit and improvement Bar_chart

--- bar_chart.py
+++ bar_chart.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def bar_chart(names,values_1,values_2):
+def bar_chart(names, std,titlex,titley):
     N =len(names)
-    values_1 = tuple(values_1)
-    values_2 = tuple(values_2)
-    std=[]
+   # values_1 = tuple(values_1)
+  #  values_2 = tuple(values_2)
+   # std=[]
     ind = np.arange(N)
     width = 0.35 
     fig, ax = plt.subplots()
-    for i, j in zip(values_1, values_2):
-        std.append((j/i)*100)
+  #  for i, j in zip(values_1, values_2):
+ #       std.append((j/i)*100)
+    print(std)
     rects1 = ax.bar(ind, tuple(std), width, color='lawngreen',edgecolor="red",linewidth=0.2)
-    ax.set_ylabel('Percentage')
-    ax.set_title('People with teacher')
+    ax.set_ylabel(titley)
+    ax.set_title(titlex)
     ax.set_xticks(ind)
     ax.set_xticklabels(names)
     autolabel(rects1, ax)
